94fbfe79bbff1a84297628044335d36e2ee67ae7
JainAnkita3 <jain.ankee3@gmail.com>
2016-02-10 20:03:42 -0600

    Renaming Basic Calculator Dir to CalculatorBasic

---
+++ src/com/test/calculatorBasic/CalculatorDemo.java
@@ -0,0 +1,28 @@
+package com.test.calculatorBasic;
+
+/*
+ * This Program will have Main method for Calculator.
+ * @author ank
+ */
+
+public class CalculatorDemo {
+
+	public static void main(String[] args) {
+
+		System.out.println("Calculator");
+
+		/*
+		 * UserInputViaConsole ui = new UserInputViaConsole();
+		 * ui.InputViaConsole(ScentificCal.numbers);
+		 */
+		Calculator c = new RegularCal();
+		c.sum(20, 30);
+		c.difference(10, 5);
+		c.product(15, 10);
+
+		ScentificCal sc = new ScentificCal();
+		sc.cube(10);
+		sc.square(20);
+	}
+
+}
