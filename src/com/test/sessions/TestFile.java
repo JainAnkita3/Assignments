d01144aa03a65539186af72baf40f83d440a0dea
JainAnkita3 <jain.ankee3@gmail.com>
2016-02-16 21:32:57 -0600

    Session trials folder

---
+++ src/com/test/sessions/TestFile.java
@@ -0,0 +1,37 @@
+package com.test;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class TestFile {
+
+	public static void main(String[] args) {
+
+		// To create Folder and than file inside it
+		File f = new File("temp1.txt");
+		File f1 = new File("temp.txt");
+
+		/*
+		 * f.mkdir(); System.out.println(f.getAbsolutePath()); try { if
+		 * (!f1.exists()) { f1.createNewFile(); System.out.println(
+		 * "File created succesfully"); //f. } else { System.out.println(
+		 * "File already exist"); }
+		 * 
+		 * } catch (IOException e) { // TODO Auto-generated catch block
+		 * e.printStackTrace(); }
+		 */
+		// First delete the file always before deleting folder
+		// f1.delete();
+		// f.delete();
+
+		/*
+		 * File files[] = f.listFiles(); // for(int i= 0; i< files.length; i++)
+		 * // Enhanced for loop for (File file : files) {
+		 * System.out.println(file + "is a File : " + file.isDirectory()); }
+		 */
+
+	}
+
+}
