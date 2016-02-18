6a03793e6af58fb7d7d45aeea4095fdc8312b672
JainAnkita3 <jain.ankee3@gmail.com>
2016-02-18 16:44:32 -0600

    Modified File Sys Assignment, using IO pkgs to copy files

--- src/com/test/fileSystem/CopyBulkFiles.java
+++ src/com/test/fileSystem/CopyBulkFiles.java
@@ -1,21 +1,26 @@
 package com.test.fileSystem;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.channels.FileChannel;
 
 /**
- * This Class is responsible to Copy Bulk Files from 1 to another location
+ * This Class is responsible to Copy Bulk Files from 1 to another location If
+ * target doesn't exist it should get created first Using IO & NIO packages
  * 
  * @author ank
  *
  */
 public class CopyBulkFiles {
 
-	private String sourceFolderPath = "//Users//rinkeshshah//Documents//Ankee//workspace//Assignments//src//com//test//fileSystem//source";
-	private String targetFolderPath = "//Users//rinkeshshah//Documents//Ankee//workspace//Assignments//src//com//test//fileSystem//target";
+	private String sourceFolderPath = "//Users//rinkeshshah//Documents//Ankee//workspace//Assignments//src//com//test//fileSystem//source.txt";
+	private String targetFolderPath = "//Users//rinkeshshah//Documents//Ankee//workspace//Assignments//src//com//test//fileSystem//target.txt";
 	private File[] sourceList;
 	private int n;
 
@@ -23,7 +28,7 @@ public class CopyBulkFiles {
 		File sourceFolder = new File(sourceFolderPath);
 		File targetFolder = new File(targetFolderPath);
 
-		n = copyFiles(sourceFolder, targetFolder);
+		n = copyFilesIO(sourceFolder, targetFolder);
 		System.out.println("Total Files copied : " + n);
 	}
 
@@ -35,9 +40,68 @@ public class CopyBulkFiles {
 	 * @return
 	 * @throws IOException
 	 */
-	private int copyFiles(File sourceFolder, File targetFolder) throws IOException {
+	private int copyFilesIO(File sourceFolder, File targetFolder) throws IOException {
 		System.out.println("Source : '" + sourceFolder.getName() + "'");
 		System.out.println("Target : '" + targetFolder.getName() + "'");
+		BufferedWriter bw = null;
+
+		if (sourceFolder.isDirectory()) {
+			System.out.println("Is Directory");
+			// If Target folder doesn't exist, create it
+			targetFolder.mkdir();
+			sourceList = sourceFolder.listFiles();
+			n = sourceList.length;
+			System.out.println("No of Files in Source folder to be copied : " + sourceList.length);
+
+			for (File sourceFile : sourceList) {
+				System.out.println("File found : " + sourceFile.getName());
+				File targetFile = new File(targetFolder, sourceFile.getName());
+				copyFilesIO(sourceFile, targetFile);
+			}
+		}
+		// If source isn't Directory,copy Source file into Destination file
+		else {
+			// Setting n to 1, if Only file needs to copy
+			if (n == 0) {
+				n = 1;
+			}
+			System.out.println("Copying files");
+			String line = null;
+			// If target file doesn't exist it will create the target file first
+			FileReader fr = new FileReader(sourceFolder);
+			BufferedReader br = new BufferedReader(fr);
+			FileWriter fw = new FileWriter(targetFolder);
+
+			while (((line = br.readLine()) != null) && (targetFolder.canWrite())) {
+				System.out.println("Write : '" + line + "' with permission as : " + targetFolder.canWrite());
+				bw = new BufferedWriter(fw);
+				bw.write(line);
+				// To put data in New line
+				bw.newLine();
+				// To avoid Overwriting of data in Target file
+				bw.flush();
+
+			}
+			System.out.println("Closing the resources");
+			br.close();
+			bw.close();
+		}
+		return n;
+
+	}
+
+	/**
+	 * Not in Use This method will copies the folder and files using NIO
+	 * packages (FileChannel)
+	 * 
+	 * @param sourceFolder
+	 * @param targetFolder
+	 * @return
+	 * @throws IOException
+	 */
+	@SuppressWarnings("unused")
+	private int copyFilesNIO(File sourceFolder, File targetFolder) throws IOException {
+		BufferedWriter bw = null;
 
 		if (sourceFolder.isDirectory()) {
 			// If Target folder doesn't exist, create it
@@ -49,18 +113,19 @@ public class CopyBulkFiles {
 			for (File sourceFile : sourceList) {
 				System.out.println("File found : " + sourceFile.getName());
 				File targetFile = new File(targetFolder, sourceFile.getName());
-				copyFiles(sourceFile, targetFile);
+				copyFilesNIO(sourceFile, targetFile);
 			}
 		}
-		// If source isn't Directory, copy the source file into Destination
-		// files
+		// If source isn't Directory, copy source file into Destination file
 		else {
 			// Setting n to 1, if Only file needs to copy
 			if (n == 0) {
 				n = 1;
 			}
-			System.out.println("Copying files");
+
+			@SuppressWarnings("resource")
 			FileChannel in = new FileInputStream(sourceFolder).getChannel();
+			@SuppressWarnings("resource")
 			FileChannel out = new FileOutputStream(targetFolder).getChannel();
 			System.out.println("Input channel Size : " + in.size());
 			System.out.println("Out channel Size :  " + out.size());
@@ -77,11 +142,9 @@ public class CopyBulkFiles {
 			}
 			if (in.isOpen() || out.isOpen()) {
 				System.out.println("In/Out is not closed properly");
-
 			}
 		}
 		return n;
-
 	}
 
 	public static void main(String[] args) {
